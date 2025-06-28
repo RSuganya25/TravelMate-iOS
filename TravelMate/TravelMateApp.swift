85cb3f6d728aca2cd25c9a098ab52bba5672f795
RSuganya25 <suganyar82@gmail.com>
2025-06-27 20:49:29 -0600

    Initial Commit

---
+++ TravelMate/TravelMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelMateApp.swift
+//  TravelMate
+//
+//  Created by Suganya Ravindran on 6/27/25.
+//
+
+import SwiftUI
+
+@main
+struct TravelMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
